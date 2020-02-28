494ea620c348b9eaf2f7509388ae5ffaf95aae24
Carlos Eustáquio <caeu227@gmail.com>
2020-02-28 14:20:11 -0300

    create www folder

---
+++ www/precache-manifest.962f7dae47e099966a8438a36e6c8c82.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18460fba76da5c2edc19426348fd1db7",
+    "url": "./index.html"
+  },
+  {
+    "revision": "df7d6dc734d97c64dbdc",
+    "url": "./static/css/2.a1840750.chunk.css"
+  },
+  {
+    "revision": "4dbe11a3f5cb17c33ce3",
+    "url": "./static/css/main.55deb68a.chunk.css"
+  },
+  {
+    "revision": "df7d6dc734d97c64dbdc",
+    "url": "./static/js/2.2358853a.chunk.js"
+  },
+  {
+    "revision": "1524314dfbff3225265120505d38e6aa",
+    "url": "./static/js/2.2358853a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4dbe11a3f5cb17c33ce3",
+    "url": "./static/js/main.7599490c.chunk.js"
+  },
+  {
+    "revision": "d838022f889c0910cb35",
+    "url": "./static/js/runtime-main.fc2c2ff8.js"
+  },
+  {
+    "revision": "2fc0de6cc036ca89a6015536fb949379",
+    "url": "./static/media/arrow-back.2fc0de6c.svg"
+  },
+  {
+    "revision": "dca2cc9ccf754fd2f4c5d2b1eaa41b06",
+    "url": "./static/media/arrow-next.dca2cc9c.svg"
+  },
+  {
+    "revision": "206ce5a6d8705f3c5138ea2ae914ba3d",
+    "url": "./static/media/back.206ce5a6.png"
+  },
+  {
+    "revision": "d637063ef882f46d661526b5a0a27b3e",
+    "url": "./static/media/close.d637063e.svg"
+  }
+]);
